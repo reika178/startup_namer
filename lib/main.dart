16f4af121e6f69b95b5b2d7a5b1502f92279cdf9
reika178 <rechiko178@gmail.com>
2020-07-16 08:30:25 +0900

    Write Your First Flutter App,part2
    7.Change the UI using themes

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext content) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
